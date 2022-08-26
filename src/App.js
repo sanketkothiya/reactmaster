64700cddbf07abe0ff472086ee0e54a8114bc69d
sanket kothiya <63290489+sanketkothiya@users.noreply.github.com>
2022-08-26 09:47:22 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -10,35 +10,62 @@ import About from './components/About'
 function App() {
   const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
 
-  const toggleMode = () => {
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+  // const toggleMode = () => {
+  //   if (mode === 'light') {
+  //     setMode('dark');
+  //     document.body.style.backgroundColor = '#042743';
+  //   }
+  //   else {
+  //     setMode('light');
+  //     document.body.style.backgroundColor = 'white';
+  //   }
+  // }
+
+  const removeBodyClasses = () => {
+    document.body.classList.remove('bg-light')
+    document.body.classList.remove('bg-dark')
+    document.body.classList.remove('bg-warning')
+    document.body.classList.remove('bg-danger')
+    document.body.classList.remove('bg-success')
+  }
+
+  const toggleMode = (cls) => {
+    removeBodyClasses();
+    console.log(cls)
+    document.body.classList.add('bg-' + cls)
     if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
     }
     else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+      // document.title = 'TextUtils - Light Mode';
     }
   }
   return (
     <>
       <Router>
-        {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
-        {/* <Navbar/> */}
+
         <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
         <Routes>
           <Route path="/about" element={<About />} />
-
           <Route path="/" element={<TextForm heading="Enter the text to analyze below" mode={mode} />} />
-
-
         </Routes>
 
-
-        {/* <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-        <div className="container my-3">
-          <TextForm heading="Enter the text to analyze below" mode={mode} />
-        </div> */}
       </Router>
     </>
 
